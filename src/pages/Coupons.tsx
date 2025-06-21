ae3edee30f669a6452148a3de05fbdfe92baff6b
fongpn <fongpn@gmail.com>
2025-06-21 19:35:30 +0800

    Updated Coupons.tsx

--- src/pages/Coupons.tsx
+++ src/pages/Coupons.tsx
@@ -149,7 +149,7 @@ export function Coupons() {
                   : 'text-gray-500 hover:text-gray-700 hover:bg-gray-50'
               }`}
             >
-              Sold Coupons ({soldCoupons.length})
+              List of Coupons ({soldCoupons.length})
             </button>
             <button
               onClick={() => setActiveTab('templates')}
